77e95ce288e02b77bcaadbe5463b9e8b9054d1ec
Yoojia.chen <chenyoca@gmail.com>
2013-06-08 18:17:39 +0800

    Change: 调整CellViewBuilder

--- library/src/net/yoojia/dataprovider/support/AsynchronousQueryTask.java
+++ library/src/net/yoojia/dataprovider/support/AsynchronousQueryTask.java
@@ -2,38 +2,33 @@ package net.yoojia.dataprovider.support;
 
 import android.database.Cursor;
 import android.os.AsyncTask;
-import android.widget.CursorAdapter;
 
 /**
- * 异步查询数据库
- * @author 桥下一粒砂 (chenyoca@gmail.com)
- * date   2013-4-7
+ * author : 桥下一粒砂 (chenyoca@gmail.com)
+ * date   : 2013-06-08
+ * 异步查询
  */
-public class AsynchronousQueryTask extends AsyncTask<Void, Void, Cursor> {
-	
-	public interface QueryTaskDelegate {
-		Cursor doQueryInBackground();
-	}
+public abstract class AsynchronousQueryTask extends AsyncTask<Void, Void, Cursor> {
 
-	private Cursor usingCursor;
-	private CursorAdapter adapter;
-	private QueryTaskDelegate invoker;
+	protected Cursor usingCursor;
+	protected CursorQueryDelegate delegate;
 
-	public AsynchronousQueryTask (CursorAdapter adapter, QueryTaskDelegate invoker) {
-		this.adapter = adapter;
-		this.invoker = invoker;
-		this.usingCursor = adapter.getCursor();
+	public AsynchronousQueryTask (Cursor usingCursor,CursorQueryDelegate delegate) {
+		this.delegate = delegate;
+		this.usingCursor = usingCursor;
 	}
 
 	protected Cursor doInBackground(Void... params) {
-		return invoker.doQueryInBackground();
+		return delegate.doQueryInBackground();
 	}
 
 	protected void onPostExecute(Cursor newCursor) {
-		adapter.changeCursor(newCursor);
+		changeCursor(newCursor);
 		if (usingCursor != null) {
 			usingCursor.close();
 		}
 		usingCursor = newCursor;
 	}
+
+	protected abstract void changeCursor(Cursor newCursor);
 }
